9393258c340ea43a808596d5f2c129b1338eb4ff
liubowen <812072237@qq.com>
2021-12-01 14:10:32 +0800

    study leetcode

--- src/main/java/com/lbw/Heap/SortArrayDistanceLessK.java
+++ src/main/java/com/lbw/Heap/SortArrayDistanceLessK.java
@@ -1,5 +1,8 @@
 package com.lbw.Heap;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 /**
  * @author admin
  * @version 1.0.0
@@ -18,4 +21,53 @@ package com.lbw.Heap;
  * @createTime 2021年11月30日 15:35:00
  */
 public class SortArrayDistanceLessK {
+
+    public static void sortedArrDistanceLessK(int[] arr,int k){
+
+        if (k == 0) {
+            return;
+        }
+
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
+        int index = 0;
+
+        for (; index <= Math.min(arr.length - 1, k - 1); index++) {
+            heap.add(arr[index]);
+        }
+        int i = 0;
+        for (; index < arr.length; i++, index++) {
+            heap.add(arr[index]);
+            arr[i] = heap.poll();
+        }
+        while (!heap.isEmpty()) {
+            arr[i++] = heap.poll();
+        }
+    }
+
+    // for test
+    public static int[] randomArrayNoMoveMoreK(int maxSize, int maxValue, int K) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        // 先排个序
+        Arrays.sort(arr);
+        // 然后开始随意交换，但是保证每个数距离不超过K
+        // swap[i] == true, 表示i位置已经参与过交换
+        // swap[i] == false, 表示i位置没有参与过交换
+        boolean[] isSwap = new boolean[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            int j = Math.min(i + (int) (Math.random() * (K + 1)), arr.length - 1);
+            if (!isSwap[i] && !isSwap[j]) {
+                isSwap[i] = true;
+                isSwap[j] = true;
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+        }
+        return arr;
+    }
+
+
 }
